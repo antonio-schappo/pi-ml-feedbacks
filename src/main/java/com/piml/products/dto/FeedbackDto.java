9655f30e08caac1aeffd1e4fb97e06e803453937
Antônio Schappo <antonio.schappo@mercadolivre.com>
2022-05-05 14:47:38 -0300

    feature: correct dto structure

--- src/main/java/com/piml/products/dto/FeedbackDto.java
+++ src/main/java/com/piml/products/dto/FeedbackDto.java
@@ -14,6 +14,7 @@ import lombok.*;
 public class FeedbackDto {
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Long id;
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Long productId;
     private Long buyerId;
     private Long starRating;
